9cdadda5cdcbde4eeb3e73c0332d0381c45b48dd
Thomas Gaspar <thomaserikgaspar@hotmail.com>
2024-06-21 20:29:17 -0300

    Sexto commit, implementado mensagens de vencedor na tela

--- Scripts/main.js
+++ Scripts/main.js
@@ -56,10 +56,10 @@ function checkWinCondition() {
         let b3Child = b3.childNodes[0].className;
         if(b1Child == "x" && b2Child == "x" && b3Child == "x") {
             //x ganhou
-            console.log("X Venceu")
+            declareWinner("x")
         }else if(b1Child == "o" && b2Child == "o" && b3Child == "o") {
             //o ganhou
-            console.log("O Venceu")
+            declareWinner("o")
         }
     }
     //horizontal meio
@@ -69,10 +69,10 @@ function checkWinCondition() {
         let b6Child = b6.childNodes[0].className;
         if(b4Child == "x" && b5Child == "x" && b6Child == "x") {
             //x ganhou
-            console.log("X Venceu")
+            declareWinner("x")
         }else if(b4Child == "o" && b5Child == "o" && b6Child == "o") {
             //o ganhou
-            console.log("O Venceu")
+            declareWinner("o")
         }
     }
     //horizontal inferior
@@ -82,10 +82,10 @@ function checkWinCondition() {
         let b9Child = b9.childNodes[0].className;
         if(b7Child == "x" && b8Child == "x" && b9Child == "x") {
             //x ganhou
-            console.log("X Venceu")
+            declareWinner("x")
         }else if(b7Child == "o" && b8Child == "o" && b9Child == "o") {
             //o ganhou
-            console.log("O Venceu")
+            declareWinner("o")
         }
     }
     //vertical esquerda
@@ -95,10 +95,10 @@ function checkWinCondition() {
         let b7Child = b7.childNodes[0].className;
         if(b1Child == "x" && b4Child == "x" && b7Child == "x") {
             //x ganhou
-            console.log("X Venceu")
+            declareWinner("x")
         }else if(b1Child == "o" && b4Child == "o" && b7Child == "o") {
             //o ganhou
-            console.log("O Venceu")
+            declareWinner("o")
         }
     }
     //vertical meio
@@ -108,10 +108,10 @@ function checkWinCondition() {
         let b8Child = b8.childNodes[0].className;
         if(b2Child == "x" && b5Child == "x" && b8Child == "x") {
             //x ganhou
-            console.log("X Venceu")
+            declareWinner("x")
         }else if(b2Child == "o" && b5Child == "o" && b8Child == "o") {
             //o ganhou
-            console.log("O Venceu")
+            declareWinner("o")
         }
     }
     //vertical direita
@@ -121,10 +121,10 @@ function checkWinCondition() {
         let b9Child = b9.childNodes[0].className;
         if(b3Child == "x" && b6Child == "x" && b9Child == "x") {
             //x ganhou
-            console.log("X Venceu")
+            declareWinner("x")
         }else if(b3Child == "o" && b6Child == "o" && b9Child == "o") {
             //o ganhou
-            console.log("O Venceu")
+            declareWinner("o")
         }
     }
     //diagonal 1
@@ -134,10 +134,10 @@ function checkWinCondition() {
         let b9Child = b9.childNodes[0].className;
         if(b1Child == "x" && b5Child == "x" && b9Child == "x") {
             //x ganhou
-            console.log("X Venceu")
+            declareWinner("x")
         }else if(b1Child == "o" && b5Child == "o" && b9Child == "o") {
             //o ganhou
-            console.log("O Venceu")
+            declareWinner("o")
         }
     }
     //diagonal 2
@@ -147,10 +147,10 @@ function checkWinCondition() {
         let b7Child = b7.childNodes[0].className;
         if(b3Child == "x" && b5Child == "x" && b7Child == "x") {
             //x ganhou
-            console.log("X Venceu")
+            declareWinner("x")
         }else if(b3Child == "o" && b5Child == "o" && b7Child == "o") {
             //o ganhou
-            console.log("O Venceu")
+            declareWinner("o")
         }
     }
     //velha
@@ -160,7 +160,36 @@ function checkWinCondition() {
             counter++;
         }
         if (counter == 9) {
-            console.log("Deu velha")
+            declareWinner(" ")
         }
+    }
+}
+
+//função que limpa o jogo, declara o vencedor e atualiza o placar
+function declareWinner(winner) {
+    let scoreboardX = document.querySelector("#scoreboard-1");
+    let scoreboardO = document.querySelector("#scoreboard-2");
+    let msg = " ";
+    if(winner == "x") {
+        scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1;
+        msg = "O jogador 1 Venceu."
+    }
+    else if(winner == "o") {
+        scoreboardO.textContent = parseInt(scoreboardO.textContent) + 1;
+        msg = "O jogador 2 Venceu"
+    }
+    else {
+        msg = "Deu velha!"
+    }
+    messageText.innerHTML = msg;
+    messageContainer.classList.remove("hide");
+    setTimeout(function() {
+        messageContainer.classList.add("hide")
+    }, 3000);
+    player1 = 0;
+    player2 = 0;
+    let boxesToRemove = document.querySelectorAll(".box div");
+    for(let i = 0; i < boxesToRemove.length; i++) {
+        boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
     }
 }
